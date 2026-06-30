314858d9db5903ef8c18b0b87f3dd24224324c0b
niyangit <niyansrikruthika06@gmail.com>
2026-06-30 17:11:37 +0530

    Add Aiven CA certificate

--- db/main.py
+++ db/main.py
@@ -1,17 +1,30 @@
+from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from utils.common import get_env_variable_value
 
-mysql_database_url=get_env_variable_value("MYSQL_DATABASE_URL")
+mysql_database_url = get_env_variable_value("MYSQL_DATABASE_URL")
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+CA_CERT = BASE_DIR / "certs" / "ca.pem"
 
-engine = create_engine(mysql_database_url)
+engine = create_engine(
+    mysql_database_url,
+    connect_args={
+        "ssl": {
+            "ca": str(CA_CERT)
+        }
+    }
+)
 
-Sessionlocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Sessionlocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
-Base=declarative_base()
-
+Base = declarative_base()
 
 def get_db_instance():
     db = Sessionlocal()
